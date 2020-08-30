1f411efedd849d7d0cb5e3a9ea7143c2ef378b82
xinyi.kao <xinyi.kao@titansoft.com.sg>
2020-08-30 23:37:16 +0800

    Add message form

--- src/messages/messageForm.vue
+++ src/messages/messageForm.vue
@@ -1,6 +1,20 @@
 <template>
     <div>
-        MessageForm
+        <div class="messageForm">
+            <form>
+                <div class="input-group mb-3">
+                    <input name="message" id="message" placeholder="Write something" class="form-control mt-3" autofocus>
+                
+                    <div class="input-group-append">
+                        <button class="btn btn-primary mt-3" type="button">&nbsp; Send &nbsp;</button>
+                    </div>
+
+                    <div class="input-group-append">
+                        <button class="btn btn-warning mt-3" type="button">Upload</button>
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
 </template>
 
@@ -9,3 +23,21 @@
         name: 'MessageForm'
     }
 </script>
+
+<style scoped>
+    .messageForm {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 67%;
+        z-index: 100;
+        color: white;
+        text-align: center;
+        margin-bottom: -16px;
+        margin-left: 33.3%;
+    }
+
+    input, button {
+        height: 50px;
+    }
+</style>
