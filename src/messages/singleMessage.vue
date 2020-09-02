1b92f241794ab5ab7b2263a97816e467582d66f0
xinyi.kao <xinyi.kao@titansoft.com.sg>
2020-09-02 23:17:22 +0800

    Implement message display

--- src/messages/singleMessage.vue
+++ src/messages/singleMessage.vue
@@ -1,6 +1,19 @@
 <template>
     <div>
-        SingleMessage
+        <div class="mt-3">
+            <div v-for="message in messages" v-bind:key="message">
+                <div class="media">
+                    <img :src="message.user.avatar" height="50" class="align-self-start mr-3">
+
+                    <div class="media-body">
+                        <h6 class="mt-0">
+                            <a href="#">{{ message.user.name }}</a> - {{ message.timestamp }}
+                        </h6>
+                        <p>{{ message.content }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
